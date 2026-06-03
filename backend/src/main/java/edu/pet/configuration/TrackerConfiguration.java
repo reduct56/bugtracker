59feddce28eaf61b6a33b677f349267ea4bb19f2
Vyacheslav <slavyan4265@gmail.com>
2026-06-03 15:14:04 +0700

    moved configuration to its own folder

--- backend/src/main/java/edu/pet/configuration/TrackerConfiguration.java
+++ backend/src/main/java/edu/pet/configuration/TrackerConfiguration.java
@@ -1,4 +1,4 @@
-package edu.pet;
+package edu.pet.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
